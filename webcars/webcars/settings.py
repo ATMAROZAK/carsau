fc050b695157ffadb8d5ec1a3d4c2227f4900440
Ibragimov Vadim <ib_vadim@mail.ru>
2018-04-30 10:35:56 +0500

    postgresql

--- webcars/webcars/settings.py
+++ webcars/webcars/settings.py
@@ -74,12 +74,15 @@ WSGI_APPLICATION = 'webcars.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "d9o6mmggl2b4i1",
+        "USER": "vnrqhulaazepei",
+        "PASSWORD": "3308bf0c890b7f722017961c92972ca7f0ea42472512867b66af5386e33191ae",
+        "HOST": "ec2-54-83-1-94.compute-1.amazonaws.com",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
