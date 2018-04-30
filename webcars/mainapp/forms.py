a0b7c6f5cc11695e930ed874a95bd19ed5088bac
Ibragimov Vadim <ib_vadim@mail.ru>
2018-04-30 14:58:16 +0500

    Update register page

--- webcars/mainapp/forms.py
+++ webcars/mainapp/forms.py
@@ -1,11 +1,20 @@
-from registration.forms import RegistrationForm
+from registration.forms import RegistrationFormUniqueEmail
 from django import forms
 
 
-class MyRegForm(RegistrationForm):
+class MyRegForm(RegistrationFormUniqueEmail):
     username = forms.CharField(max_length=254, required=False, widget=forms.HiddenInput())
+    first_name = forms.CharField(max_length=20, required=True)
 
     def clean_email(self):
         email = self.cleaned_data['email']
         self.cleaned_data['username'] = email
         return email
+
+    def save(self, commit=True):
+        user = super(MyRegForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        if commit:
+            user.save()
+
+        return user
--- webcars/mainapp/urls_default.py
+++ webcars/mainapp/urls_default.py
@@ -3,12 +3,21 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
+from registration.backends.default.views import RegistrationView
+from mainapp.forms import MyRegForm
+
+
+class RegistrationViewUniqueEmail(RegistrationView):
+    form_class = MyRegForm
 
 urlpatterns = [
 
     url(r'^$',
         TemplateView.as_view(template_name='index.html'),
         name='index'),
+
+    url(r'^register/$', RegistrationViewUniqueEmail.as_view(),
+        name='registration_register'),
 
     url(r'^accounts/',
         include('registration.backends.default.urls')),
