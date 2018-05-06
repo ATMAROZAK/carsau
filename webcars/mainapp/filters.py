480115eeda92b644b590129e0c45c688d45d2a3d
Ibragimov Vadim <ib_vadim@mail.ru>
2018-05-06 22:33:42 +0500

    add case insensitive in search field

--- webcars/mainapp/filters.py
+++ webcars/mainapp/filters.py
@@ -3,6 +3,8 @@ import django_filters
 
 
 class CarFilter(django_filters.FilterSet):
+    make = django_filters.CharFilter(lookup_expr='icontains')
+    car_model = django_filters.CharFilter(lookup_expr='icontains')
     class Meta:
         model = Car
         fields = ['make', 'car_model', 'year', 'price']
