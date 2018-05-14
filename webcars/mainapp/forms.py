c62fc4a6c6c6d14810a63c4d4f605daca858877e
Ibragimov Vadim <ib_vadim@mail.ru>
2018-05-14 18:12:37 +0500

    Bug fix

--- webcars/mainapp/forms.py
+++ webcars/mainapp/forms.py
@@ -38,7 +38,7 @@ class CarSearchForm(forms.Form):
 
 class SimpleCarSearchForm(forms.Form):
 
-    make = forms.ModelChoiceField(queryset=CarMake.objects.all(), to_field_name="make")
+    make = forms.ModelChoiceField(queryset=CarMake.objects.all(), to_field_name="make", required=True)
     car_model = forms.ModelChoiceField(queryset=CarModel.objects.none(), to_field_name="car_model")
     price = forms.IntegerField()
     year = forms.IntegerField()
@@ -52,19 +52,23 @@ class AdvancedCarSearch(forms.Form):
     color = forms.CharField()
 
     def __init__(self, *args, **kwargs):
-        make = kwargs.pop('make')
+
+        make = None
+        if 'make' in kwargs:
+            make = kwargs.pop('make')
 
         car_model = None
         if 'car_model' in kwargs:
             car_model = kwargs.pop('car_model')
-        print(car_model)
+
         color = None
         if 'color' in kwargs:
             color = kwargs.pop('color')
 
         super(AdvancedCarSearch, self).__init__(*args, **kwargs)
-        self.fields['make'].initial = CarMake.objects.get(make=make)
-        self.fields['car_model'].queryset = CarModel.objects.filter(make__make=make)
+        if make:
+            self.fields['make'].initial = CarMake.objects.get(make=make)
+            self.fields['car_model'].queryset = CarModel.objects.filter(make__make=make)
 
         if car_model:
             self.fields['car_model'].initial = CarModel.objects.get(car_model=car_model)
